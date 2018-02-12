84c4073c9c90baf27d8c6e9560ea2dcd12e73d90
TedaLIEz <aliezted@gmail.com>
2018-02-12 12:40:41 +0800

    feat: io read_file

--- include/io.h
+++ include/io.h
@@ -7,10 +7,30 @@
 
 #include <string>
 #include <fstream>
+#include <vector>
 namespace io {
 inline bool file_exist(const std::string& file_path) {
   std::ifstream file(file_path);
   return file.good();
+}
+
+inline bool read_file(const std::string& file_path, unsigned char*& data, long& file_size) {
+  if (!file_exist(file_path)) {
+    std::cerr << "file not exist!" << std::endl;
+    return false;
+  }
+  std::ifstream file(file_path, std::ios::binary);
+  if (!file.is_open()) {
+    std::cerr << "Error opening file!" << std::endl;
+    return false;
+  }
+  file.seekg(0, std::ios::end);
+  file_size = static_cast<long>(file.tellg());
+  data = new unsigned char[file_size];
+  file.seekg(0, std::ios::beg);
+  file.read(reinterpret_cast<char *>(data), file_size);
+  file.close();
+  return true;
 }
 }
 #endif //SIMPLEH264_UTIL_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include "bitutil.h"
+#include "io.h"
 int main() {
-  auto * bits = new unsigned char[3];
-  bits[0] = 170;
-  bits[1] = 85;
-  bits[2] = 255;
-  std::bitset<8> a((int)bits[0]);
-  std::bitset<8> b((int)bits[1]);
-  std::bitset<8> c((int)bits[2]);
-  std::cout << a;
-  std::cout << b;
-  std::cout << c << std::endl;
-  std::cout << std::bitset<13>(bit::next_bit(bits, 3, 13, 5)) << std::endl;
+  long file_size;
+  unsigned char *data;
+  if (io::read_file("sample/test_1080p.264", data, file_size)) {
+    std::cout << "file size " << file_size << std::endl;
+    for (int i = 0; i < 10; i++) {
+      std::cout << std::hex << +*(data + i) << std::endl;
+    }
+    delete[] data;
+  }
   return 0;
 }
