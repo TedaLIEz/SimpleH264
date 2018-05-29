82f4c93b5d325e6e0a4cfc9129e005885409f676
TedaLIEz <aliezted@gmail.com>
2018-05-29 10:34:47 +0800

    feat: define nal types

--- include/h264/nalu.h
+++ include/h264/nalu.h
@@ -6,6 +6,19 @@
 #define SIMPLEH264_NAL_H
 #define NAL_HEADER_SHORT 0x000001
 #define NAL_HEADER_LONG 0x00000001
+#define NAL_TYPE_UNSPECIFIED 0
+#define NAL_TYPE_SLICE 1
+#define NAL_TYPE_SLICE_A 2
+#define NAL_TYPE_SLICE_B 3
+#define NAL_TYPE_SLICE_C 4
+#define NAL_TYPE_SLICE_IDR 5
+#define NAL_TYPE_SEI 6
+#define NAL_TYPE_SPS 7
+#define NAL_TYPE_PPS 8
+#define NAL_TYPE_PIC_DELIMITER 9
+#define NAL_TYPE_END_OF_SEQ 10
+#define NAL_TYPE_END_OF_STREAM 11
+#define NAL_TYPE_FILTER_DATA 12
 #include <vector>
 #include <fstream>
 #include <ostream>
